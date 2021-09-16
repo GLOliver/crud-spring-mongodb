6c5125f1619eab9f8a86cc45393c0129479e5bbd
GLOliver <gui.lopes11@gmail.com>
2021-09-15 23:22:20 -0300

    Server On

--- src/main/java/com/gloliver/spring/data/mongodb/repository/TutorialRepository.java
+++ src/main/java/com/gloliver/spring/data/mongodb/repository/TutorialRepository.java
@@ -1,4 +1,11 @@
 package com.gloliver.spring.data.mongodb.repository;
 
-public class TutorialRepository {
+import com.gloliver.spring.data.mongodb.model.Tutorial;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+    List<Tutorial> findByTitleContaining(String title);
+    List<Tutorial> findByPublished(boolean published);
 }
